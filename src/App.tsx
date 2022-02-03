b194009a20033fc51b7e681966dc276d2adf35ac
Giovanni Ortega <giovanni.ortega@wizeline.com>
2022-02-03 09:40:30 -0500

    feat: Firebase setup, retrieving data from db

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import db from './firebase/firebaseConfig';
+
+interface MyObject {
+  email: number;
+  password: string;
+}
 
 function App() {
+  // const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getDocs(collection(db, 'users'));
+      const usersArray: MyObject[] = [];
+      data.forEach((user) => {
+        const userData = user.data();
+        usersArray.push(userData);
+        console.log('array', usersArray);
+      });
+      // setUsers(usersArray);
+    };
+
+    getData();
+  }, []);
   return (
     <div className="App">
       <h1>Hola</h1>
